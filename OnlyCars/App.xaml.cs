84953dad55046146f6f186f9e8085e5582eb5c03
CHATO <colochocristian72@gmail.com>
2024-09-04 21:25:21 -0600

    Agregar archivos de proyecto.

---
+++ OnlyCars/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlyCars
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
