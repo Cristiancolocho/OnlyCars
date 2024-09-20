0150761b321049bdd4e47440f1a442e355435597
CHATO <colochocristian72@gmail.com>
2024-09-19 23:36:45 -0600

    Modificando codigo

---
+++ OnlyCars/Views/StoresView.xaml.cs
@@ -0,0 +1,44 @@
+using OnlyCars.Models;
+
+namespace OnlyCars.Views;
+
+public partial class StoresView : ContentPage
+{
+	public List<Store> Stores { get; set; }
+	public StoresView()
+	{
+		InitializeComponent();
+		LoadData();
+		BindingContext = this;
+	}
+
+    private void LoadData()
+    {
+		Stores = new List<Store>
+		{
+			new Store
+			{
+				Id = 1,
+				Header="header",
+				Logo="b.png",
+				Name = "Dodge Challenger 1970",
+				km = "315km/h",
+				Money  = 45000,
+                Stock = 100,
+				Rating = 4.5
+			},
+			new Store
+			{
+				Id = 1,
+				Header="",
+				Logo="logo.png",
+				Name = "Nissan Skyline GTR R34",
+				km = "315km/h",
+				Money = 5,
+                Stock = 10,
+				Rating = 4.5
+			},
+		};
+		        
+    }
+}
